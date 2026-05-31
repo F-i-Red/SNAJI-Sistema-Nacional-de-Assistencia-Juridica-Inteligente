e5a559e38db41f7e5db6adbb3ee321ff68dfad35
F.Red <fredgsfm@gmail.com>
2026-05-31 02:07:43 +0100

    Add files via upload

---
+++ backend/app/argument_tree.py
@@ -0,0 +1,11 @@
+
+class ArgumentTree:
+
+    def build(self):
+
+        return {
+            "root_argument": {},
+            "supporting_arguments": [],
+            "counter_arguments": [],
+            "conflicts": []
+        }

69884fd9d15da9773a3cc0065ad75353f7ed4bf8
F.Red <fredgsfm@gmail.com>
2026-05-31 02:09:14 +0100

    Add files via upload

---
+++ backend/app/bm25_retriever.py
@@ -0,0 +1,11 @@
+
+class BM25Retriever:
+
+    def search(self, query: str):
+
+        return [
+            {
+                "score": 0.91,
+                "document": "Artigo relevante BM25"
+            }
+        ]
